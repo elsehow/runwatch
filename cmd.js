2b60247d011c1755bee079cb9682a20c20c20ec4
elsehow <yes@cosmopol.is>
2016-04-29 17:27:09 -0700

    fixed glob recognition bug by switching to native os/*nix glob handling

--- cmd.js
+++ cmd.js
@@ -3,31 +3,33 @@ function quit (err) {
   console.log(err)
   process.exit(1)
 }
-function usage () {
-  quit(`USAGE: 
-
-  runwatch [files] -r "[command-to-run]"
-
-  ex,
-    
-    runwatch **/*.js -r "tape test/*.js"`)
-}
+//function usage () {
+//  quit(`USAGE:
+//
+//  runwatch [files] -r "[command-to-run]"
+//
+//  ex,
+//
+//    runwatch **/*.js -r "tape test/*.js"`)
+//}
 var argv = require('minimist')(process.argv.slice(2))
 var gaze = require('gaze')
 var spawn = require('child_process').spawn
-var glob = argv._[0]
+var files = argv._
 var cmd = argv.r
-if (!glob) usage()
+if (!files) usage()
 if (!cmd) usage()
 function spawnCommand () {
   var cmds = cmd.split(' ')
   return spawn(cmds[0], cmds.slice(1), { stdio: 'inherit', stderr: 'inherit' })
 }
-gaze(glob, (err, watcher) => {
-  if (err) quit(err)
-  var proc = spawnCommand()
-  watcher.on('changed', () => {
-    proc.kill()
-    proc = spawnCommand()
+var proc = spawnCommand()
+files.forEach(f => {
+  gaze(f, (err, watcher) => {
+    if (err) quit(err)
+    watcher.on('changed', () => {
+      proc.kill()
+      proc = spawnCommand()
+    })
   })
 })
